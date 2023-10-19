1b63a99815f3fb467b929750e15f10a476052ab3
Aaron jiles <sirjiles1230@gmail.com>
2023-10-19 17:33:55 -0500

    still trying to work out function calculations for table display

--- froyo.js
+++ froyo.js
@@ -26,11 +26,26 @@ const froyo = {
 
 console.table(froyo)
 
-function howManyOfEach(flavors){(flavors)
-  let numberOf=[]
-  for (const key in froyo)
-  for (let i = 0; i < flavors.length;i++)
-    if (flavors[i]=numberOf){
-      numberOf=flavors[i]
-    }
+function ordersFlavors(flavors){
+  (flavors)
+      let order=[]
+        for (let i = 0;i <flavors.length;i++)
+          if (flavors[i]<order){
+            order=flavors[i]}
+        console.log (orders)
 }
+  for (const key in froyo)
+    {
+      froyo.numberOf= []
+    }
+console.table (froyo)
+
+  function numberOfEach(flavors){(flavors)
+  let number=froyo.numberOf
+    for (let i=0;i<flavors.length;i++)
+      if (flavors[i]<number)
+        number=flavors[i]
+  }
+  for (const key in froyo) 
+      if (froyo.numberOf > 0)
+       froyo.numberOf =[orders]
